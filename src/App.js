9a9e68654899c8413aadb6447b5e0593e57faf88
Behruz <meliboyevbehruz008@gmail.com>
2022-08-15 16:06:14 +0500

    Header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import './App.css';
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
+import Main from "./layout/Main";
 
 function App() {
   return (
-    <div className="App">
-    
-    </div>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/layout/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#" class="brand-logo">Logo</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="#">Movie</a></li>
+                </ul>
+            </div>
+      </nav>
+    )
+}
